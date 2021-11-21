c308ac08ad14a4c5bb38f857753024a5d7ec0fbb
Anton Khorkov <akhorkov@bk.ru>
2021-11-21 17:36:48 +0000

    Add Google Test

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,23 @@
 #include <iostream>
 
-int main(int argc, char **argv) {
-	std::cout << "Hello world!" << std::endl;
-	return 0;
+
+#include "gtest/gtest.h"
+#include "gmock/gmock.h"
+
+class ProjTest : public ::testing::Test
+{
+public:
+};
+
+TEST_F(ProjTest, Hello)
+{
+
 }
+
+
+
+
+//int main(int argc, char **argv) {
+//	std::cout << "Hello world!" << std::endl;
+//	return 0;
+//}
